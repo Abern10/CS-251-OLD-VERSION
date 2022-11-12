52ee377aea50d4cbe65616c155538cdd261a2c8f
Alexander Bernatowicz <bernatowicz1122@gmail.com>
2022-11-12 12:17:52 -0600

    idk again

---
+++ build/CMakeFiles/CS251-Project5.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/AlexBernatowicz/Desktop/CS 251/CS251-Project5/tests.cpp" "CMakeFiles/CS251-Project5.dir/tests.cpp.o" "gcc" "CMakeFiles/CS251-Project5.dir/tests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/AlexBernatowicz/Desktop/CS 251/build/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/Users/AlexBernatowicz/Desktop/CS 251/build/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CS251-Project5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS251-Project5.dir/tests.cpp.o"
+  "CMakeFiles/CS251-Project5.dir/tests.cpp.o.d"
+  "CS251-Project5"
+  "CS251-Project5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS251-Project5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
